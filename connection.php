49575003d62c05a107a3c38f648a9eccf2ded35d
faith-mkandawire <131264649+faith-mkandawire@users.noreply.github.com>
2023-07-20 00:31:06 +0200

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username= "root";
-$password = "";
-$db = "my_project";
-$con = mysqli_connect($servername,$username,$password,$db)or die("failed to connect".mysqli_connect_error());
-
-?>
